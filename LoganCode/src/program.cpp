65df2225203d21f58d582128bba664b8d58e8088
Brandon Dao <Brandon.Dao@GMR.local>
2024-05-25 17:29:37 -0700

    square drive

--- LoganCode/src/program.cpp
+++ LoganCode/src/program.cpp
@@ -19,6 +19,7 @@ public:
     const double diameterRobot = 254.43;
     ev3dev::medium_motor* Left;
     ev3dev::medium_motor* Right;
+    ev3dev::color_sensor* LeftColor;
     bool MoveToDegree(int degreeTarget)
     {
         return  (Left->position() > degreeTarget) || (-Right->position() > degreeTarget);
@@ -32,37 +33,37 @@ public:
 
         return  (Amount > degreeTarget);
     }
-    RobotDefition(ev3dev::medium_motor* Left, ev3dev::medium_motor* Right)
+    bool MoveToColor(int brightness)
+    {
+        return  (brightness == LeftColor->reflected_light_intensity());
+    }
+    RobotDefition(ev3dev::medium_motor* Left, ev3dev::medium_motor* Right,ev3dev::color_sensor* LeftColor)
     {
         this->Left = Left;
         this->Right = Right;
+        this->LeftColor = LeftColor;
     }
 
 
 };
 int main()
 {
-    ev3dev::color_sensor* test =  new ev3dev::color_sensor(ev3dev::INPUT_1);
-    test->set_mode(ev3dev::color_sensor::mode_col_reflect);
-
-    while(true)
-    {
-        std::cout << test->reflected_light_intensity() << std::endl; //god is dead
-    }
+   
     ev3dev::medium_motor* Right =  new ev3dev::medium_motor(ev3dev::OUTPUT_B);
     ev3dev::medium_motor* Left = new ev3dev::medium_motor(ev3dev::OUTPUT_C);
+    ev3dev::color_sensor* LeftColor = new ev3dev::color_sensor(ev3dev::INPUT_1);
     Right->set_polarity(ev3dev::medium_motor::polarity_inversed);
     Left->set_polarity(ev3dev::medium_motor::polarity_normal);
-    RobotDefition s{Left,Right};
-    int amountCommands = 8;
+    RobotDefition s{Left,Right,LeftColor};
+    int amountCommands = 1;
 
 
     std::function<bool(int)> MoveToAngle = std::bind(&RobotDefition::MoveToDegree, &s, std::placeholders::_1);
     std::function<bool(int)> TurnToDegree = std::bind(&RobotDefition::TurnToDegree, &s, std::placeholders::_1);
-
+    std::function<bool(int)> MoveToColor = std::bind(&RobotDefition::MoveToColor, &s, std::placeholders::_1);
     std::shared_ptr<Command> Commands[8];
 
-    std::shared_ptr<Move> move(new Move(75, 75, MoveToAngle,200,Left,Right));
+    std::shared_ptr<Move> move(new Move(75, 75, MoveToColor,9,Left,Right));
     std::shared_ptr<Turn> turn(new Turn(50, -50, TurnToDegree, 90, Left, Right));
 
     Commands[0] = move;
